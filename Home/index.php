012565160f476e81f8abeb04a079d0790283b4d3
krhandmadepaper <sanjay.krenterprises@gmail.com>
2023-07-20 02:32:42 +0530

    blank

--- Home/index.php
+++ Home/index.php
@@ -6,7 +6,6 @@
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <?php include"navbar.php" ?>
   </head>
   <body style ="background-color:rgba(164, 189, 250, 0.288) ;">
